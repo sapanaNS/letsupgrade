b30c5f224c64467d6d60b68183572da2e4cc7224
sapanaNS <62982654+sapanaNS@users.noreply.github.com>
2020-09-01 17:02:17 +0530

    Add files via upload

---
+++ letsupgrade.js
@@ -0,0 +1,32 @@
+
+// Question 1
+// Program to search for a particular character in a String
+// var str="hello world ";
+// var res=str.charAt(7);
+//  console.log(res);
+
+//Question 2
+// program to convert mintues to seconds
+//  let min=60;
+//  var sec=min*60;
+//  var seconds=Math.ceil(sec);
+//  console.log(seconds);
+
+//Question 3
+// Program to search for a element in array of string 
+// var fruits=["Banana","Orange","apple","Mango"];
+//  console.log(fruits.slice(2,3));
+
+// Questioin 4
+// Program to display only elements containing 'a' in them from a array
+//  const arr=["canada","india","butan","sapana"];
+//  for(let i=0;i<arr.length;i++){
+//      if(arr[i].includes('a')){
+//          console.log(arr[i]);
+//      }
+//  }
+
+// Question 5
+// Print an array in reverse order
+ // var fruits=["Banana","Orange","apple","Mango"];
+// console.log(fruits.reverse());
